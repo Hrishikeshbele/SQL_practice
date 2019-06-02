c1ad5bba287fe5b82777fd1119ba3f119b5e2bb7
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-06-02 12:59:59 +0530

    Create UNION.sql

---
+++ UNION.sql
@@ -0,0 +1,16 @@
+'''
+combinining different quaries results using union operator and adding label to each queries.
+'''
+SELECT  customer_id,first_name,points,'Bronze' AS type
+FROM sql_store.customers
+WHERE points<2000
+UNION
+SELECT  customer_id,first_name,points,'Silver' AS type
+FROM sql_store.customers
+WHERE points BETWEEN 2000 AND 3000
+UNION 
+SELECT  customer_id,first_name,points,'Gold' AS type
+FROM sql_store.customers
+WHERE points>3000
+ORDER BY first_name
+
