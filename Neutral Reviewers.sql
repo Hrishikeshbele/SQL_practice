935e80ed97a041f0d7a2dc144a723cba397ff53f
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-23 18:06:40 +0530

    Update Neutral Reviewers.sql

--- Neutral Reviewers.sql
+++ Neutral Reviewers.sql
@@ -1,7 +1,8 @@
-'''
+/*
 Write a SQL Query to find the name of all reviewers who have rated their ratings with a NULL value.
 
-'''
+*/
+
 /* we first selected all reviewers_id whose rating is null from ratings table and then selected the name 
   corresponding these id's */
 select reviewer_name 
