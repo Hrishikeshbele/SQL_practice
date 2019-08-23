0a3eafd2157220f4a15e8091842e74a79c8e2a76
hrishikesh bele <Hrishikeshbele@users.noreply.github.com>
2019-08-23 11:42:08 +0530

    Create Neutral Reviewers.sql

---
+++ Neutral Reviewers.sql
@@ -0,0 +1,9 @@
+'''
+Write a SQL Query to find the name of all reviewers who have rated their ratings with a NULL value.
+
+'''
+/* we first selected all reviewers_id whose rating is null from ratings table and then selected the name 
+  corresponding these id's */
+select reviewer_name 
+from reviewers 
+where reviewer_id in(select reviewer_id from ratings where reviewer_stars is NULL)
